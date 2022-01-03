deff8e1aebae6cd433df39de6f327688525e353d
Aaron R <aaronr919@gmail.com>
2022-01-03 00:29:28 -0800

    Delete 2020 directory

--- 2020/2020-12-06.py
+++
@@ -1,9 +0,0 @@
-
-with open('inputs/2020-12-06.txt') as raw_votes_data:
-    votes_data = [vote.replace('\n', '')
-                  for vote in raw_votes_data.read().split('\n\n')]
-
-sum_of_counts = 0
-for vote in votes_data:
-    sum_of_counts += len(set(vote))
-print(sum_of_counts)
